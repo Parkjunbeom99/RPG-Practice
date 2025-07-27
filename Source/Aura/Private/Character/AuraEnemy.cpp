a09b58064104039d278ad778719e9cefb2ce1277
Parkjunbeom99 <pjb990@gmail.com>
2025-07-27 15:33:38 +0900

    InitActor이라는 abilitysystemcomponent에 있는걸 활용하여 owner actor와 owner avatar지정

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,11 +2,23 @@
 
 
 #include "Character/AuraEnemy.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 #include "Aura/Aura.h"
 
 AAuraEnemy::AAuraEnemy()
 {
 	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
+	
+	
 }
 
 void AAuraEnemy::BeginPlay()
@@ -14,6 +26,9 @@ void AAuraEnemy::BeginPlay()
 	Super::BeginPlay();
 	GetMesh()->SetRenderCustomDepth(false);
 	Weapon->SetRenderCustomDepth(false);
+
+	AbilitySystemComponent->InitAbilityActorInfo(this,this);
+
 }
 
 void AAuraEnemy::HighLightActor()
